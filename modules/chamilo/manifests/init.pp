d7082da5cc0ae8e89f3e45127e6d28ecf3d44d66
Julien Pivotto <roidelapluie@inuits.eu>
2012-03-06 13:08:19 +0100

    updates for shared folders

--- modules/chamilo/manifests/init.pp
+++ modules/chamilo/manifests/init.pp
@@ -8,9 +8,30 @@ class chamilo ($version = 'stable'){
     $get = 'get-all'
   }
   file {
+    "/var/www/$folder/files":
+      ensure  => directory,
+      mode    => '0777',
+      require => Exec['clone-chamilo-modules'],
+  }
+  file {
+    "/var/www/$folder/common/configuration":
+      ensure  => directory,
+      mode    => '0777',
+      require => Exec['clone-chamilo-modules'],
+  }
+  file {
     "/var/www/$folder":
       ensure  => directory,
       require => Class['apache'],
+  }
+
+  file {
+   '/root/.hgrc':
+     ensure  => present,
+     content => "[paths]\ndefault = https://bitbucket.org/chamilo/${folder}\n",
+     require => Package['mercurial'],
+
+
   }
 
   apache::site {
@@ -23,7 +44,8 @@ class chamilo ($version = 'stable'){
       command => "hg clone https://bitbucket.org/chamilo/${folder} /var/www/${folder}",
       creates => "/var/www/${folder}/index.php",
       path    => '/usr/bin',
-      require => [Package['mercurial'], File["/var/www/${folder}"]],
+      require => [File['/root/.hgrc'], File["/var/www/${folder}"], Package['php']],
+      timeout => 0,
   }
   exec {
     'clone-chamilo-modules':
@@ -31,7 +53,7 @@ class chamilo ($version = 'stable'){
       creates => "/var/www/${folder}/install/index.php",
       cwd     => "/var/www/${folder}",
       path    => '/usr/bin',
-      require => [Package['php'], Exec['clone-chamilo']],
+      require => [File['/root/.hgrc'], File["/var/www/${folder}"], Package['php']],
       timeout => 0,
   }
   php::module { ['gd', 'xml', 'mbstring', 'mysql']: }
